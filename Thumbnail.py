b77406a5d2113ee8aa09cafa902d6c3bebca30c5
Jake Cullum <jakesgamig@gmail.com>
2020-06-03 20:09:21 +0100

    Menu
    Added menu for users to choose directory to scan for images to turn into thumbnails

--- Thumbnail.py
+++ Thumbnail.py
@@ -4,7 +4,25 @@ from PIL import Image
 DefaultLocation = r"C:\Users\jake\Videos\Dota 2"
 DefaultSize = 180,180
 
-
+def Menu():
+    option = 0
+    while (option != 1):
+        print("""
+1 - exit
+0 - Create thumbnails
+""")
+        user = input("Enter your choice: ")
+        while user != "0" and user != "1":
+            user = input("Enter your choice: ")
+        if(user == "0"):
+            while(os.path.isdir(user) != True):
+                user = input("Enter file directory to scan: ")
+            #add size code here if you want users to be able to choose size on thumbnails
+            Thumbnail(user,DefaultSize)
+        else:
+            option = 1
+            
+        os.system('cls' if os.name == 'nt' else 'clear')
 
 def Thumbnail(location,size):
     ThumbnailLocation = location + r"\Thumbnails"
@@ -27,4 +45,4 @@ def CreateThumbnail(imageloc,size,newLoc):
     im.thumbnail(size,Image.ANTIALIAS)
     im.save(newLoc + "\\" + os.path.basename(imageloc))
 
-Thumbnail(DefaultLocation,DefaultSize)
+Menu()
